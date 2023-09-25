8de51bfcbd7c5f158200b53994f93f524992fa56
Jacob <Jacobskittles000@gmail.com>
2023-09-25 10:40:42 -0700

    separated scraping function from NER functions

---
+++ chatwork/ner.py
@@ -0,0 +1,35 @@
+import nltk
+from nltk.text import Text
+
+def textify(filename):
+    # read text
+    text = open(filename, "r").read()
+    tokens = nltk.word_tokenize(text)
+    textList = Text(tokens)
+    return textList
+
+class namedEntity :
+    def __init__(self, word:str) -> None:
+        '''Takes in a word and created a new object with blank alias and timeline fields to track mentions'''
+        self.__repr__ = word
+        self.name = word
+        self.alias = set()
+        self.timeline = []
+
+    def add_alias(self, word:str):
+        self.alias.add(word)
+
+    def add_event(self, message:str):
+        self.timeline.append(message)
+
+
+def extract_ne(quote:str):
+    '''Iterates through a text or quote and returns a set of named entities as a list of strings'''
+    words = nltk.word_tokenize(quote)
+    tags = nltk.pos_tag(words)
+    tree = nltk.ne_chunk(tags, binary=True)
+    return set(
+        " ".join(i[0] for i in t)
+        for t in tree
+        if (hasattr(t, "label") and t.label == "NE") or (hasattr(t, "label") and t.label == "NNP")
+    )
--- chatwork/scrape.py
+++ chatwork/scrape.py
@@ -1,7 +1,5 @@
 import nltk
 import re
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.corpus import stopwords
 from nltk.text import Text
 import sys
 
@@ -12,7 +10,7 @@ USERNAMEEXPRESSION = "<[A-Za-z0-9]+>" # RegEx for usernames contained between <>
 SYSTEMUSERNAME = "-!-" # The tag for system messages, change based on system/chat log origins
 TIMESTAMPEXPRESSION = "\[[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]{1,3})?\]" # RegEx for timestamp as [HH:MM:SS.MS]
 KEYWORDS = [ # List of Keywords to reference
-    'keyboard', 'key'
+    'torchlight2', 'tl2'
 ]
 
 ######################################################################################
@@ -39,41 +37,7 @@ KEYWORDS = [ # List of Keywords to reference
 text = open('chatmessages.txt', 'r').read()
 quote = "Joe Biden is The President of the United States. He has many important duties." # open('chatmessages.txt', 'r').read()
 
-stop_words = set(stopwords.words("english"))
 
-
-def textify(filename):
-    # read text
-    text = open(filename, "r").read()
-    tokens = word_tokenize(text)
-    textList = Text(tokens)
-    return textList
-
-class namedEntity :
-    def __init__(self, word:str) -> None:
-        '''Takes in a word and created a new object with blank alias and timeline fields to track mentions'''
-        self.__repr__ = word
-        self.name = word
-        self.alias = set()
-        self.timeline = []
-
-    def add_alias(self, word:str):
-        self.alias.add(word)
-
-    def add_event(self, message:str):
-        self.timeline.append(message)
-
-
-def extract_ne(quote:str):
-    '''Iterates through a text or quote and returns a set of named entities as a list of strings'''
-    words = nltk.word_tokenize(quote)
-    tags = nltk.pos_tag(words)
-    tree = nltk.ne_chunk(tags, binary=True)
-    return set(
-        " ".join(i[0] for i in t)
-        for t in tree
-        if (hasattr(t, "label") and t.label == "NE") or (hasattr(t, "label") and t.label == "NNP")
-    )
-
-Text(nltk.word_tokenize(text)).concordance("torchlight2")
-
+textList = Text(nltk.word_tokenize(text))
+for word in KEYWORDS:
+    textList.concordance(word)
